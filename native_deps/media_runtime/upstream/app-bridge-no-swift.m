fa7fabe2916df8b9f1708830d9778d9f3d79713a
yk3372 <yk3372@gmail.com>
2026-08-01 00:16:37 +0800

    fix: release unused no-Swift mpv client

--- native_deps/media_runtime/upstream/app-bridge-no-swift.m
+++ native_deps/media_runtime/upstream/app-bridge-no-swift.m
@@ -5,6 +5,6 @@
 void cocoa_init_media_keys(void) {}
 void cocoa_uninit_media_keys(void) {}
 void cocoa_set_input_context(struct input_ctx *input_context) {}
-void cocoa_set_mpv_handle(struct mpv_handle *ctx) {}
+void cocoa_set_mpv_handle(struct mpv_handle *ctx) { mpv_destroy(ctx); }
 void cocoa_init_cocoa_cb(void) {}
 int cocoa_main(int argc, char *argv[]) { return 0; }
